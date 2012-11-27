ad106b7274c124565ec599531502e0374c0c442f
Roman Golovin <roman199494@mail.ru>
2012-11-27 20:30:21 +0400

    Second attempt

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@
 using namespace std;
 typedef unsigned char uchar;
 typedef vector<uchar> text;
+enum dir {ENCODE, DECODE};
 //typedef unsigned int uint;
 
 void help(){
@@ -36,8 +37,8 @@ int arrSize(char arr[]) {
 //@param int key - key of encoding
 //@param int pow - nunber of symbols in alphabet
 //@param bool direction - direct or reverse coding (direct when true)
-int encode(int sym, int key, int pow, bool direction){
-	if (direction) {return abs((sym+key)%pow);}
+int findNewSymbolPosition(int sym, int key, int pow, dir direction){
+	if (direction==ENCODE) {return abs((sym+key)%pow);}
 	else {return abs((sym-key+pow)%pow);};
 }
 
@@ -61,15 +62,52 @@ bool fileExists(const char *fname)
 			  return access(fname, 0) != -1;
 			}
 
+//Read characters from file wich name is "filename" to array "fileText"
+//
+//@param char *filename - name of input file
+//@param text &fileText - link to array for input
+void readFile(char *filename, text &fileText) {
+FILE *f;
+int *buf=new int();
+f=fopen(filename,"r");
+while (*buf!=EOF) {
+	*buf=fgetc(f);
+	fileText.push_back(*buf);
+}
+fclose(f);
+delete(buf);
+}
+
+//encode characters in array "alphaLink" by "key" and "direction"
+//
+//@param text &fileText - link to array with symbols for encoding
+//@param char *alphaLink - link to array with alphabet
+//@param int size - nubmer of symbols in alphabet
+//@param int key - key of encoding
+//@param dir direction - ENCODE or DECODE
+void encodeSymbols(text &fileText,char *alphaLink, int size, int key, dir direction){
+long textSize=fileText.size();
+int *symbol=new int();
+for (long i=0;i<textSize; i++) {
+	if (charNum(fileText.at(i),alphaLink,size)!=-1) {
+		*symbol=charNum(fileText.at(i),alphaLink,size);
+
+		fileText.at(i)=alphaLink[findNewSymbolPosition(*symbol, key, size, direction)];
+	}
+}
+delete(symbol);
+}
 
 int main(int argc, char *argv[]) {
-if (argc==1) {return -1;};
+
+if (argc==1) {incorrectInput(); return -1;};
 int alphabet=0; //Contants adress of cell with alphabet in argv[]. If default alphabet is enable then contants 0;
 int key=0; //contants key of encoding;
 int inFile=0; //Contants adress of cell with name of input file in argv[];
 int outFile=0; //Contants adress of cell with with name of output file in argv[]. If default file is using then contants 0;
 bool alphabetInput=false; //Did user input alphabet with "--aplphabet="?
-bool direction=1;
+dir direction=ENCODE;
+
 
 for (int i=1;i<argc;i++) {
 
@@ -77,7 +115,7 @@ for (int i=1;i<argc;i++) {
 		
 		if (argv[i][1]=='a')  { if (argv[i+1][0]!='-') {alphabet=i+1; i++;} else {incorrectInput(); return 0;} };
 
-		if (argv[i][1]=='t') {if (argv[i+1][0]!='-') {if (argv[i+1][0]=='e') {direction=1;} else {if (argv[i+1][0]=='d') {direction=0;} else {incorrectInput(); return 0;} } i++;}};
+		if (argv[i][1]=='t') {if (argv[i+1][0]!='-') {if (argv[i+1][0]=='e') {direction=ENCODE;} else {if (argv[i+1][0]=='d') {direction=DECODE;} else {incorrectInput(); return 0;} } i++;}};
 
 		if (argv[i][1]=='h') {help(); return 0;};
 
@@ -85,7 +123,7 @@ for (int i=1;i<argc;i++) {
 
 				if (argv[i][2]=='a') {alphabet=i; alphabetInput=true;};
 
-				if (argv[i][2]=='t') {if (argv[i][7]=='e') {direction=1;} else {if (argv[i][7]=='d') {direction=0;} else {incorrectInput(); return 0;} } };
+				if (argv[i][2]=='t') {if (argv[i][7]=='e') {direction=ENCODE;} else {if (argv[i][7]=='d') {direction=DECODE;} else {incorrectInput(); return 0;} } };
 
 				if (argv[i][2]=='h') {help(); return 0;}
 
@@ -114,7 +152,7 @@ else {if (alphabet!=0) {size=sizeof(argv[alphabet]);} else {size=62;} };
 char alphabetArr[size];
 char *alphaLink=&alphabetArr[0];
 
-if (key>size) {printf("%s\n", "Key must be less than length of alphabet"); return 0;};
+while (key>size) {key-=size;};
 
 
 //Fill array by symbols
@@ -143,38 +181,20 @@ else {
 	};
 
 
-//Reading of input file by character to array
-FILE * inF;
-char cbuf;
-text *fileText=new text();
-int *buf=new int();
-inF=fopen(argv[inFile],"r");
-while (*buf!=EOF) {
-	*buf=fgetc(inF);
-	fileText->push_back(*buf);
-}
-fclose(inF);
-delete(buf);
+//Compare to work with files
+text *fileText=new text();//array with characters from file
+char *options=argv[inFile];
 
-
-//encode characters in array
-long textSize=fileText->size();
-int *symbol=new int();
-for (long i=0;i<textSize; i++) {
-	if (charNum(fileText->at(i),alphaLink,size)!=-1) {
-		*symbol=charNum(fileText->at(i),alphaLink,size);
-
-		fileText->at(i)=alphabetArr[encode(*symbol, key, size, direction)];
-	}
-}
-delete(symbol);
+readFile(options,*fileText);
+encodeSymbols(*fileText,alphaLink,size,key,direction);
 
 
 //Create output file
+FILE * inF;
+long textSize=fileText->size();
 if (outFile!=0) {inF=fopen(argv[outFile],"w");}
 else {inF=fopen("result.txt","w");};
 for (long i=0;i<textSize-1;i++) {fputc(fileText->at(i),inF);}
-
 fclose(inF);
 
 printf("%s","Complete. Result is in ");
